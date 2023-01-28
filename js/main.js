f170357af83220acf16989fb4a5d4289f5d92b22
Yyoungshine <Christiansunday19@gmail.com>
2023-01-28 23:12:49 +0100

    Table modification

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    } );
-} );
+let copy = document.getElementsByClassName("buttons-copy");
+
+
+
+
+// $(document).ready(function() {
+//     $('#example').DataTable( {
+//         dom: 'Bfrtip',
+//         buttons: [
+//             'copy', 'csv', 'excel', 'pdf', 'print'
+//         ]
+//     } );
+
+//     copy.classList.add("btn-secondary");
+// } );
+
+
+$('#example').DataTable({
+    paging: false,
+    "info": false,
+    searching: false,
+    dom: 'Bfrtip',
+    buttons: [
+        { extend: 'copy', className: 'btn btn-secondary text' },
+        { extend: 'csv', className: 'btn btn-primary glyphicon glyphicon-save-file' },
+        { extend: 'excel', className: 'btn btn-primary glyphicon glyphicon-list-alt' },
+        { extend: 'pdf', className: 'btn btn-primary glyphicon glyphicon-file' },
+        { extend: 'print', className: 'btn btn-primary glyphicon glyphicon-print' }
+    ]
+    });
+
 
 // jQuery(document).ready(function () {
 //     my_table = jQuery("#myTable").DataTable();
